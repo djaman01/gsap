a0b93c4f10018e6e7d64c5f74a1e70babcdd6ce6
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-03-30 15:46:45 +0000

    scroll trigger

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,5 +1,37 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+//Gsap scrollTrigger is a plugin: so we need to add this code to make it work
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
-  // TODO: Implement the gsap scroll trigger
+  //We need to define a reference
+  const scrollRef = useRef();
+
+  useGSAP(() => {
+    const boxes = gsap.utils.toArray(scrollRef.current.children); //permet de cibler chacune des box rose et orange
+
+    boxes.forEach((e) => {
+      //e=element=les 2 div avec boxx roses et oranges
+      gsap.to(e, {
+        //au lieu d'utiliser une class ou id pour cibler, on cible avec e=element grace à forEach
+        x: 350,
+        rotation: 360,
+        borderRadius: "100%",
+        duration: 2,
+        scale: 1.5,
+        //Ici on va utiliser le scrollTrigger pour que l'animation ne s'active que quand on arrive à son niveau après avoir scroll
+        scrollTrigger: {
+          trigger: e,
+          toggleActions: "restart pause reverse pause", //possible d'écrire: play/pause/resume/restart/reset/complete/none
+          //1er:Ce qu'il se passe quand l'élement entre dans le viewport / 2eme: Quand on dépasse le viewport et qu'on ne voit plus l'élement /3ème: Quand on revient à l'élement après l'avoir dépassé/4ème: quand on revient en haut de la page et qu'on dépasse l'élement (pause ou restart est bien ici)
+        },
+        ease: "power1.inOut",
+      });
+    });
+  }, []);
 
   return (
     <main>
@@ -50,8 +82,8 @@ const GsapScrollTrigger = () => {
           <path d="M5 12l7 7 7-7" />
         </svg>
       </div>
-
-      <div className="mt-20 w-full h-screen">
+      {/* Avec useRef on va cibler le div qui rassemble les 2 box rose et orange */}
+      <div className="mt-20 w-full h-screen" ref={scrollRef}>
         <div
           id="scroll-pink"
           className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
@@ -61,6 +93,7 @@ const GsapScrollTrigger = () => {
           className="scroll-box w-20 h-20 rounded-lg bg-orange-500"
         />
       </div>
+      <div className="mt-96"></div>
     </main>
   );
 };
